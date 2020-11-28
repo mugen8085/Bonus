113b88bc8926638ffed2ad41c9f542f76c32250d
Berbun <mugen8085@gmail.com>
2020-11-28 22:08:25 +0800

    儲存CSV功能

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 # import package
+from datetime import datetime as dtime
 from datetime import date,timedelta
 from urllib.request import urlopen
 from dateutil import rrule
@@ -23,9 +24,21 @@ def craw_one_month(stock_number,date):
     return pd.DataFrame(data['data'],columns=data['fields'])
 
 
+# 根據使用者輸入的日期，以月為單位，重複呼叫爬取月股價的函式
+def craw_stock(stock_number, start_month):
+    b_month = date(*[int(x) for x in start_month.split('-')])
+    now = dtime.now().strftime("%Y-%m-%d")  # 取得現在時間
+    e_month = date(*[int(x) for x in now.split('-')])
+
+    result = pd.DataFrame()
+    for dt in rrule.rrule(rrule.MONTHLY, dtstart=b_month, until=e_month):
+        result = pd.concat([result, craw_one_month(stock_number, dt)], ignore_index=True)
+        time.sleep(2000.0 / 1000.0);
+
+    return result
 
 
 
 if __name__ == '__main__':
-    df = craw_one_month(2330, "2017-01-01")
-    print(df)
+    df = craw_stock(3034,"2020-10-18")
+    df.to_csv('3034.csv', encoding='utf_8_sig')

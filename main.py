d3d1bd5fd559d4e6715018eb18d055d5e38ec45e
Berbun <mugen8085@gmail.com>
2020-12-06 20:46:51 +0800

    DB Test

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 import json
 import time
-
+import psycopg2
 
 # 爬取每月股價的目標網站並包裝成函式
 def craw_one_month(stock_number,date):
@@ -40,5 +40,36 @@ def craw_stock(stock_number, start_month):
 
 
 if __name__ == '__main__':
-    df = craw_stock(3034,"2020-10-18")
-    df.to_csv('3034.csv', encoding='utf_8_sig')
+    # df = craw_stock(3034,"2020-10-18")
+    # df.to_csv('3034.csv', encoding='utf_8_sig')
+
+    # Update connection string information
+    # host = "127.0.0.1"
+    # dbname = "Stock001"
+    # user = "postgres"
+    # password = "roger0"
+    # sslmode = "require"
+    #
+    # conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
+    # conn = psycopg2.connect(conn_string)
+
+    conn = psycopg2.connect(database="Stock001", user="postgres", password="roger0", host="127.0.0.1", port="5432")
+    print("Connect DB success.")
+    cur = conn.cursor()
+
+    cur.execute("DROP TABLE IF EXISTS COMPANY;")
+    print("Finished dropping table (if existed)")
+
+    cur.execute('''CREATE TABLE COMPANY
+               (ID INT PRIMARY KEY     NOT NULL,
+               NAME           TEXT    NOT NULL,
+               AGE            INT     NOT NULL,
+               ADDRESS        CHAR(50),
+               SALARY         REAL);''')
+    print("Create table success.")
+
+
+
+    conn.commit()
+    cur.close()
+    conn.close()
